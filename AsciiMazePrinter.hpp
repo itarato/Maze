c70a77740b618fcab63ac11f153102b8aa1cc449
Peter Arato <it.arato@gmail.com>
2017-02-04 23:17:38 +0800

    Docs.

--- AsciiMazePrinter.hpp
+++ AsciiMazePrinter.hpp
@@ -7,7 +7,7 @@ void print_maze__ascii(MazeGenerator& mg) {
 
   for (int y = 0; y < mg.getH(); y++) {
     for (int x = 0; x < mg.getW(); x++) {
-      std::cout << w[mg.getCell(x, y)];
+      std::cout << w[mg.getCell(x, y) & 0b1111];
     }
     std::cout << std::endl;
   }
--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ int main(int argc, char* argv[]) {
 
   MazeGenerator mg{static_cast<size_t>(std::stoi(argv[1])), static_cast<size_t>(std::stoi(argv[2]))};
 
-  // print_maze__ascii(mg);
-  DrawEngine de{mg, 8};
+  print_maze__ascii(mg);
+  // DrawEngine de{mg, 8};
 
   return 0;
 }
