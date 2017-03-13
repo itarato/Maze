e34ff1268c3b259fab445dfe719fb0846312b2a7
Peter Arato <it.arato@gmail.com>
2017-03-13 16:48:00 +0800

    Make draw method optional from args to avoid lame hardcoding.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
 
 #include "MazeGenerator.h"
 #include "AsciiMazePrinter.h"
@@ -12,13 +13,30 @@ int main(int argc, char* argv[]) {
     exit(1);
   }
 
-  MazeGenerator mg{static_cast<size_t>(std::stoi(argv[1])), static_cast<size_t>(std::stoi(argv[2]))};
+  size_t w = static_cast<size_t>(std::stoi(argv[1]));
+  size_t h = static_cast<size_t>(std::stoi(argv[2]));
 
-  //print_maze__ascii(mg);
-  // DrawEngine de{mg, 8};
+  int opt;
+  bool flagDrawAsciiMap = false;
+  bool flagDrawGraphicalMap = false;
+  while ((opt = getopt(argc, argv, "ag")) != -1) {
+    switch (opt) {
+      case 'a':
+        flagDrawAsciiMap = true;
+        break;
+
+      case 'g':
+        flagDrawGraphicalMap = true;
+        break;
+    }
+  }
+
+  MazeGenerator mg{w, h};
+
+  if (flagDrawAsciiMap) print_maze__ascii(mg);
+  if (flagDrawGraphicalMap) DrawEngine de{mg, 8};
 
   BasicMazeSolver bms{mg};
 
   return 0;
 }
-  
